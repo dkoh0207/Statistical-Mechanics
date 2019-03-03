5f724581e416da8403e943a7743a1de2da48f125
dae <koh0207@stanford.edu>
2019-03-03 02:15:36 -0800

    Completing z2

--- z2_3d.cpp
+++ z2_3d.cpp
@@ -9,6 +9,7 @@
 #include "boost/tuple/tuple_comparison.hpp"
 #include <cassert>
 #include <cmath>
+#include <map>
 #include <ctime>
 
 using namespace std;
@@ -25,17 +26,46 @@ inline int periodic(int i, const int limit, int add) {
     return (i + limit + add) % (limit);
 }
 
-
+void read_input(unsigned int&, unsigned int&, unsigned int&, 
+double&, double&, string&, vector<double>&, string &);
 double compute_free_energy(Lattice &lattice, const unsigned int lsize, 
 const unsigned int n_bonds, const double & K);
 void initialize(Lattice &lattice, const unsigned int lsize, const unsigned int n_bonds, 
-double &K, double& E, const int &coldstart);
+double &K, double& E, const int &coldstart, map<int, double> & weights);
 double deltaE(Lattice &lattice, site &bond,
 const unsigned int lsize, const unsigned int n_bonds, const double &K);
 site choose_random_bond(const unsigned int lsize, const unsigned int n_bonds);
 void metropolis(Lattice &lattice, const unsigned int lsize, 
 const unsigned int n_bonds, const double &K, double &E);
 double compute_wilson_loop(Lattice &lattice, const unsigned int lsize, const double &K);
+
+
+void read_input(unsigned int &lsize, unsigned int &num_steps, unsigned int &mcs,
+  double &init_T, double &final_T, vector<double> &sweep, string &path) {
+  // Function to read configurations for Monte Carlo pass.
+  cout << "Enter lattice size: " << endl;
+  cin >> lsize;
+  cout << "Enter initial T: " << endl;
+  cin >> init_T;
+  cout << "Enter Final T: " << endl;
+  cin >> final_T;
+  cout << "Enter number of steps: " << endl;
+  cin >> num_steps;
+  cout << "Enter MCS per site: " << endl;
+  cin >> mcs;
+  cout << "Enter output path: " << endl;
+  cin >> path;
+  double T_step = (final_T - init_T) / ((double) num_steps);
+  double t = {0.0};
+  for (auto i = 0; i != num_steps; ++i) {
+    t = init_T + T_step * (double) i;
+    sweep.push_back(t);
+  }
+  cout << "Temperature Sweep Range:" << endl;
+  for (auto k : sweep) {
+    cout << "T = " << k << endl;
+  }
+}
 
 
 double compute_free_energy(Lattice &lattice, 
@@ -69,7 +99,7 @@ const unsigned int lsize, const unsigned int n_bonds, const double & K) {
 
 
 void initialize(Lattice &lattice, const unsigned int lsize, const unsigned int n_bonds, 
-double &K, double& E, const int &coldstart) {
+double &K, double& E, const int &coldstart, map<int, double> &weights) {
     /*
     Function for initializing all bonds in the lattice.
     coldstart = 1 gives all spins in +1 config, while 
@@ -91,6 +121,13 @@ double &K, double& E, const int &coldstart) {
             }
         }
     }
+    // Initializing weights for metropolis
+    weights.insert(pair<int, double>(-12, exp(-12.0 * K)));
+    weights.insert(pair<int, double>(-8, exp(-8.0 * K)));
+    weights.insert(pair<int, double>(-4, exp(-4.0 * K)));
+    weights.insert(pair<int, double>(4, 1.0));
+    weights.insert(pair<int, double>(8, 1.0));
+    weights.insert(pair<int, double>(12, 1.0));
 
     // Compute free energy of the whole lattice
     E = compute_free_energy(lattice, lsize, n_bonds, K);
@@ -110,6 +147,8 @@ const unsigned int lsize, const unsigned int n_bonds, const double &K) {
     for (int m = 0; m != n_bonds; ++m) {
         double plaq = 0.0;
         plaq = 0;
+        // This particular implementation of computing the energy
+        // is borrowed from Michael Cruetz's z2 lattice gauge simulation.
         if (m != b) {
             plaq = lattice[ind[0]][ind[1]][ind[2]][b];
             ind[m] = periodic(ind[m], lsize, -1);
@@ -129,7 +168,7 @@ const unsigned int lsize, const unsigned int n_bonds, const double &K) {
             plaq_sum += plaq;
         }
     }
-    dE = -2.0 * plaq_sum * K;
+    dE = -2 * plaq_sum;
     return dE;
 }
 
@@ -147,7 +186,7 @@ site choose_random_bond(const unsigned int lsize, const unsigned int n_bonds) {
 }
 
 void metropolis(Lattice &lattice, const unsigned int lsize, const unsigned int n_bonds,
-const double &K, double &E) {
+const double &K, double &E, map<int, double> &weights) {
     unsigned int count_flipped = 0;
     unsigned int N = lsize*lsize*lsize*n_bonds;
     //unsigned int N = 1;
@@ -158,8 +197,11 @@ const double &K, double &E) {
         int key = 0;
         site s = choose_random_bond(lsize, n_bonds);
         dE = deltaE(lattice, s, lsize, n_bonds, K);
+        w = weights[dE];
+        cout << "w = " << endl;
+        cout << "exp(dE) = " << endl;
         //cout << "dE = " << dE << endl;
-        if (u(random_engine) < exp(dE)) {
+        if (u(random_engine) < w) {
             //cout << "Flipped" << endl;
             E += dE;
             count_flipped += 1;
@@ -188,18 +230,89 @@ double compute_wilson_loop(Lattice &lattice, const unsigned int lsize, const dou
     return wilson_loop;
 }
 
+void write_data(const unsigned int mcs, const unsigned int lsize, 
+const unsigned int n_bonds, const string& output_filename,
+Lattice& lattice, double& K, double& E, map<int, double> &weights) {
+  // Free Energy per site
+  vector<double> vec_E;
+  vector<double> vec_E2;
+  double m = 0.0;
+  double energy = 0.0;
+  int n = lsize*lsize*lsize*n_bonds;
+  for (auto i = 0; i != mcs; ++i) {
+    if (i % 1000 == 0) {
+      cout << "i = " << i << endl;
+      cout << "M = " << m << endl;
+      //test_update(lattice, lsize);
+    }
+    metropolis(lattice, lsize, n_bonds, K, E, weights);
+    energy = E / ((double) n);
+    vec_E.push_back(energy);
+    vec_E2.push_back(energy * energy);
+  }
+  ofstream result;
+  result.open(output_filename);
+  result << "E, E2" << endl;
+  for (auto i = 0; i != mcs; ++i) {
+    result << setprecision(8) << vec_E[i] << "," << vec_E2[i] << endl;
+  }
+}
+
 int main() {
-    unsigned int lsize,  num_steps, start, n_bonds = {0};
-    n_bonds = 3; // Three bonds per site for Z2 Lattice Gauge Theory in 3D
-    double init_K, final_K, M, E = {0.0};
-    double K = 0.7;
+
+    // Initializing Step
+    unsigned int lsize, num_steps, start = {0};
+    double init_K, final_K, M, E, step = {0.0};
+    double K, T = 0.0;
+    K = 0.5;
     lsize = 10;
-    unsigned int N = lsize * lsize * lsize * n_bonds; // Total number of bonds
+    unsigned int N = lsize * lsize * lsize;
     string output_filename;
-    int coldstart = 1;
-    //read_input(lsize, num_steps, start, init_K, final_K, output_filename, sweep);
+    vector<double> sweep;
+    unsigned int mcs = 2000; // NUmber of Monte Carlo steps per site.
+    unsigned int n_bonds = 3;
+    read_input(lsize, num_steps, start, init_K, 
+    final_K, output_filename, sweep, output_filename);
+
+    // Set the lattice
     Lattice lattice(boost::extents[lsize][lsize][lsize][n_bonds]);
     cout << "Lattice size = " << lattice.size() << endl;
+
+    // Set output file configurations
+    ofstream readme;
+    string s_readme = "./" + output_filename + "/readme.txt";
+    readme.open(s_readme);
+    readme << "Number of MCS: " << mcs << endl;
+    readme << "Lattice Size: " << lsize << endl;
+    readme << "K,T" << endl;
+
+    int coldstart = 1;
+    for (auto i = 0; i != num_steps; ++i) {
+      T = sweep[i];
+      K = 1.0 / T;
+      cout << "K = " << K << endl;
+      cout << "T = " << T << endl;
+      readme << setprecision(8) << K << ",";
+      readme << setprecision(8) << T << endl;
+
+      for (coldstart = -1; coldstart < 2; coldstart += 2) {
+        cout << "Coldstart: " << coldstart << endl;
+        map<int, double> weights;
+        initialize(lattice, lsize, n_bonds, K, E, coldstart, weights);
+        string ofile;
+        string ind = to_string(i);
+        ofile = "./" + output_filename + "/";
+        if (coldstart == -1){
+          ofile += "hot_";
+        } else {
+          ofile += "cold_";
+        }
+        ofile = ofile + ind + ".csv";
+        write_data(mcs, lsize, n_bonds, ofile, lattice, K, E, weights);
+      }
+    }
+
+    /*
     initialize(lattice, lsize, n_bonds, K, E, coldstart);
     unsigned int mcs = 200;
     double E_test = 0;
@@ -209,5 +322,6 @@ int main() {
         cout << "W = " << compute_wilson_loop(lattice, lsize, K) << endl;
         //cout << "E_text = " << compute_free_energy(lattice, lsize, n_bonds, K) << endl;
     }
+    */
     return 0;
 }
